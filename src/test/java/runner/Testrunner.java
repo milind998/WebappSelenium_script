07f138d5065d02daf500ab88ee51d51cc6846c08
Milind Ingale <milind.ingale@bookwater.com>
2024-12-28 15:24:29 +0530

    Removed Promo-code

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -21,7 +21,7 @@ import io.cucumber.junit.CucumberOptions;
 		monochrome = false,
 		dryRun = false,
 		features={"src/test/resources/com/features/01homePage.feature",
-				 "src/test/resources/com/features/02mainsettings.feature"
+				 "src/test/resources/com/features/02mainsettings.feature",
 				//"src/test/resources/com/features/03promoCode.feature"
 				//"src/test/resources/com/features/04transactions.feature",
 				//"src/test/resources/com/features/05SuperAdmin.feature",
