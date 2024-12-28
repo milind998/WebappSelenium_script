b1d809df84fde9b3a7f29c4d8467d141b9433c78
Milind Ingale <milind.ingale@bookwater.com>
2024-12-28 12:54:59 +0530

    Changed in Xpath

--- src/main/java/com/pages/mainsettingsPage.java
+++ src/main/java/com/pages/mainsettingsPage.java
@@ -242,9 +242,8 @@ public class mainsettingsPage extends Home {
 	}
 
 	public WebElement AddSlotsTabText1(int i) {
-		System.out.println(("//*[@class='MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputAdornedEnd css-1uvydh2'])[" + i + "]"));
-		WebElement AddSlotsText1 = ldriver.findElement(By.xpath(
-				"(//*[@class='MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputAdornedEnd css-1uvydh2'])[" + i + "]"));
+		System.out.println(("//label[contains(text(), 'Delivery Slot')])["+i+"]"));
+		WebElement AddSlotsText1 = ldriver.findElement(By.xpath("//label[contains(text(), 'Delivery Slot')])["+i+"]"));
 		return AddSlotsText1;
 	}
 
