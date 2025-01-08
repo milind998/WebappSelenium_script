3ab06c6edeaaa982fc7171cb4a62b4fd7a1eb4cc
Milind Ingale <milind.ingale@bookwater.com>
2025-01-08 15:50:40 +0530

    Added Threads

--- src/main/java/com/pages/mainsettingsPage.java
+++ src/main/java/com/pages/mainsettingsPage.java
@@ -267,6 +267,7 @@ public class mainsettingsPage extends Home {
 		//WebElement ClickOnStart = ldriver.findElement(By.xpath("(//*[@class='MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl MuiInputBase-adornedEnd css-1bn53lx'])[1]"));
 		//ClickOnStart.click();
 		DeliverySlotStartTimeBox.sendKeys(Keys.CONTROL, "a", Keys.DELETE);
+		
 		DeliverySlotStartTimeBox.sendKeys(DeliverySlotStartTime);
 	}
 
