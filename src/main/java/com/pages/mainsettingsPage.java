732664830f7a4b8006f57eef8fd09a21e1fb8181
Milind Ingale <milind.ingale@bookwater.com>
2025-01-08 15:53:54 +0530

    all

--- src/main/java/com/pages/mainsettingsPage.java
+++ src/main/java/com/pages/mainsettingsPage.java
@@ -29,7 +29,6 @@ public class mainsettingsPage extends Home {
 
 	public void settingLink() {
 		Actions actions = new Actions(ldriver);
-
 		actions.moveToElement(settingButton).click().build().perform();
 		settingButton.click();
 	}
